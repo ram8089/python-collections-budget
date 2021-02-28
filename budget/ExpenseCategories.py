635cd47baac2fa02c4d55c59f2e2562cb78820c5
ram8089 <43286412+ram8089@users.noreply.github.com>
2021-02-28 17:24:54 -0500

    complted module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -2,6 +2,7 @@ from . import Expense
 import timeit
 import matplotlib.pyplot as plt
 
+
 def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
@@ -14,30 +15,18 @@ def main():
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
 
-    print(timeit.timeit(stmt = "expenses.categorize_for_loop()",
-                  setup =
-                  ...
-    from . import Expense
-    expenses = Expense.Expenses()
-    expenses.read_expenses('data/spending_data.csv')
-    ...
-                  ,number = 100000,
-                  globals = globals()
-    ))
+    setup1 = """from . import Expense\nexpenses = Expense.Expenses()\nexpenses.read_expenses('data/spending_data.csv')"""
+    print(timeit.timeit(stmt="expenses.categorize_for_loop()", setup = setup1, number=100000, globals=globals()))
+    print(timeit.timeit(stmt="expenses.categorize_set_comprehension()", setup=setup1, number=100000, globals=globals()))
 
     fig, ax = plt.subplots()
     labels = ['Necessary', 'Food', 'Unnecessary']
-    divided_expenses_sum =[]
+    divided_expenses_sum = []
     for category_exps in divided_set_comp:
-        divided_expenses_sum.append(category_exps)
-        sum(x.amount for x in category_exps)
+        divided_expenses_sum.append(sum(x.amount for x in category_exps))
 
-    ax.pie(divided_expenses_sum, labels = labels, autopct = '%1.1f%%')
+    ax.pie(divided_expenses_sum, labels=labels,autopct='%1.1f%%')
     plt.show()
-
-
-
-
 
 
 if __name__ == "__main__":
